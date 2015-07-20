bbbacad5b094dde75251587a236a922be3bfee2c
James Quacinella <james@deemok.com>
2015-07-20 19:00:31 -0400

    Crawl code is done and output is saved

--- Final/crawler.py
+++ Final/crawler.py
@@ -1,12 +1,64 @@
-import graphlab as gl
+#import graphlab as gl
 import pickle
 import twitter
+import logging
+import time
+from collections import defaultdict
 
-import graphlab as gl
-import pickle
-import twitter
 
-# Lets create out api object w/ OAuth parameters
+### Setup a console and file logger
+
+logger = logging.getLogger('crawler')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('crawler.log')
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+### Setup signals to make sure API calls only take 60s at most
+
+from functools import wraps
+import errno
+import os
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=60, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+@timeout()
+def getFollowers(api, follower):
+    ''' Function that will get a user's list of followers from an api object. 
+    NOTE: the decorator ensures that this only runs for 60s at most. '''
+    # return api.GetFollowerIDs(follower)
+    return api.GetFriendIDs(follower)
+
+
+### Twitter API
+
+# Lets create our list of api OAuth parameters
 API_TOKENS = [
     {"consumer_key": 'yp4wi4FASXbsRKa6JxYqzhUlH',
     "consumer_secret": 'Wkh1d5ygAOp4Bp65syFzHRN4xQsS8O4FvU3zHWosX8NXCqMpcl',
@@ -69,6 +121,7 @@ API_TOKENS = [
     "requests_timeout": 60}
 ]
 
+# Now create a list of twitter API objects
 apis = []
 for token in API_TOKENS:
     apis.append( twitter.Api(consumer_key=token['consumer_key'],
@@ -78,95 +131,120 @@ for token in API_TOKENS:
                     requests_timeout=60))
 
 
-
+# The account id / screen name we want followers from
 account_screen_name = 'fairmediawatch'
 account_id = '54679731'
 
-from collections import defaultdict
+# Keep track of nodes connected to account, and all edges we need in the graph
 nodes = set()
-edges = defaultdict(list)
+edges = defaultdict(set)
 
 
-
+# Try to load first level followers from pickle;
+# otherwise, generate them from a single API call and save via pickle
 try:
-    print "Loading followers for %s" % account_screen_name
+    logger.info("Loading followers for %s" % account_screen_name)
     f = open("following1", "rb")
     following = pickle.load(f)
 except Exception as e:
-    print "Failed. Generating followers for %s" % account_screen_name
+    logger.info("Failed. Generating followers for %s" % account_screen_name)
     following = api.GetFriendIDs(screen_name=account_screen_name)
     pickle.dump(following, open("following1", "wb"))
 
+# Try to load the nodes and first level edges from pickle;
+# otherwise generate them from the 'following' list and save
 try:
-    print "Loading nodes and edges for depth = 1, for %s" % account_screen_name
+    logger.info("Loading nodes and edges for depth = 1, for %s" % account_screen_name)
     n = open("nodes.follow1.set", "rb")
     e = open("edges.follow1.dict", "rb")
     nodes = pickle.load(n)
     edges = pickle.load(e)
 except Exception as e:
-    print "Failed. Generating nodes and edges for depth = 1, for %s" % account_screen_name
+    logger.info("Failed. Generating nodes and edges for depth = 1, for %s" % account_screen_name)
     for follower in following:
         nodes.add(follower)
-        edges[account_id].append(follower)
+        edges[account_id].add(follower)
     pickle.dump(nodes, open("nodes.follow1.set", "wb"))
     pickle.dump(edges, open("edges.follow1.dict", "wb"))
 
-### Depth2
 
-# for follower, following_depth2 in following_depth_part1.iteritems():
-#     twitter_graph.add_vertex(name=str(follower))
-#     try:
-#         twitter_graph.add_vertices( [str(f) for f in following_depth2] )
-#         twitter_graph.add_edges( [(str(f), str(follower)) for f in following_depth2 ] )
-#     except Exception as e:
-#         print f
-#         raise e
 
-import time
+### Crawling for Depth2
 
+
+# Index the api list, and start from the first api object
 api_idx = 0
 api = apis[api_idx]
 
-blacklist= ["74323323"]
-#turnover = False
+# Some accounts give us issues (either too many followers or no permissions)
+blacklist= [74323323, 43532023, 19608297, 25757924, 240369959, 173634807, 17008482, 142143804]
+api_updated = False
 
-for follower in following:
+# It is nice to start from a point in the list, instead of from the beginning
+starting_point = 142143804
+if starting_point:
+    starting_point_idx = following.index(starting_point)
+    following_iter = range(starting_point_idx, len(following))
+else:
+    following_iter = range(len(following))
+
+# Try loading second layer of followers from pickle, otherwise start from scratch
+try:
+    f = open("edges.follow2.dict", "rb")
+    edges = pickle.load(f)
+    logger.info("Loaded edges.follow2 into memory!")
+except Exception as e:
+    logger.info("Starting from SCRATCH: did not load edges.follow2 into memory!")
+    pass
+
+# For each follower of the main account ...
+for follower_idx in following_iter:
+    follower = following[follower_idx]
     success = False
     
+    # ... check if they are on the blacklist; if so, skip
     if follower in blacklist:
-        print("Skipping due to blacklist")
+        logger.info("Skipping due to blacklist")
         continue
 
+    # Otherwise, attempt to get list of their followers
+    followers_depth2_list = []
     while not success:
         try:
-            print("Getting followers for follower %s" % follower)
-            followers_depth2_list = api.GetFriendIDs(follower)
+            logger.info("Getting followers for follower %s" % follower)
+            followers_depth2_list = getFollowers(api, follower)
             success = True
-            time.sleep(1)
+        except TimeoutError as e:
+            # If api call takes too long, move on
+            logger.info("Timeout after 60s for follower %d" % follower)
+            success = True      # technically not a success but setting flag so next loop moves on
+            continue
         except Exception as e:
-            print("API Exception %s; api-idx = %d" % (str(e), api_idx))
+            # IF we get here, then we hit API limits
+            logger.info("API Exception %s; api-idx = %d" % (str(e), api_idx))
             
-            if api_idx % len(API_TOKENS) == 0 and api_idx >= len(API_TOKENS): #and not turnover:
-                print("Save edges to pickle file for follower = %s" % follower)
+            # Are we at the begining of api list? 
+            # IF so, dump edges so far via pickle and sleep
+            if api_updated and api_idx % len(API_TOKENS) == 0 and api_idx >= len(API_TOKENS):
+                logger.info("Save edges to pickle file for follower = %s" % follower)
                 pickle.dump(edges, open("edges.follow2.dict", "wb"))
-                
-                print("Sleeping ...")
+                logger.info("Sleeping ...")
                 time.sleep(60)
+                api_updated = False
+            # Otherwise, move on to the next api object and try again
             else:
                 api_idx += 1
                 api = apis[api_idx % len(API_TOKENS)]
+                api_updated = True
             
     
-    print("Adding followers to the graph")
-    for follower_depth2 in followers_depth2_list:
-        if follower_depth2 in nodes:
-            edges[follower] = follower_depth2
+    # After getting the followers, find the intersection of those followers
+    # with those of the first-level followers and add to edge dict
+    if followers_depth2_list:
+        logger.info("Adding followers to the graph")
+        edges[follower].update(nodes.intersection(followers_depth2_list))
 
 
-
-print("FINAL: Adding followers to the graph")
-for follower_depth2 in followers_depth2_list:
-    if follower_depth2 in nodes:
-        edges[follower] = follower_depth2
-print("Save edges to pickle file for follower = %s" % follower)
+# Write out final list of edges via pickle
+logger.info("Save edges to pickle file for follower = %s" % follower)
 pickle.dump(edges, open("edges.follow2.dict", "wb"))
